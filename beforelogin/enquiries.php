4cda4cd8c4ec2a2aecf0b08d6ef77f880a60eefd
MSaiJagapathi <msaij@users.noreply.github.com>
2020-01-16 18:04:52 +0530

    chnages

--- beforelogin/enquiries.php
+++ beforelogin/enquiries.php
@@ -6,43 +6,43 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
-    
+
     <style>
         #enqform
         {
             margin:10px 0 0 10px;
         }
     </style>
-    
+
 <body>
     <?php
         include 'base_beforelogin.php';
     ?>
-    
+
     <div>Contact Us:</div>
-    
+
     <div id="enqform">
     <form class="enq-form" action="enq.php" method="post">
-        
+
   <div class="name">
       <label for="name">Name:</label><br>
       <input type="text" name="name" required></div>
-        
+
   <div class="phone">
       <label for="phone">Phone number:</label><br>
       <input type="tel" name="phone" pattern="[6-9]{1}[0-9]{9}"></div>
-        
+
    <div class="email">
        <label for="email">Email:</label><br>
       <input type="email" name="email" required></div>
-                
+
    <div class="query">
        <label for="query">Query(if any):</label><br>
       <textarea cols="50" rows="8"></textarea></div>
-        
+
    <div class="submit"><br>
-      <input type="submit" value="submit form"></div>
+      <button type="submit" value="submit form">Submit</button></div>
     </form>
-    </div>    
+    </div>
 </body>
 </html>
--- beforelogin/signup.php
+++ beforelogin/signup.php
@@ -9,29 +9,31 @@
     <?php
         include 'base_beforelogin.php';
     ?>
+    <link rel="stylesheet" href="jcss_forbefore/signup.css">
 <body>
-    <!-- 
+
 <div class="signupform">
     <form name="signup-form" action="signupform.php" method="post">
+
     <div><label for="firstname">First Name</label>
         <input type="text" name="firstname" required></div>
-        
+
     <div><label for="lastname">Last Name</label>
         <input type="text" name="lastname" required></div>
-        
+
     <div><label for="email">Email</label>
         <input type="text" name="email" required></div>
-        
-    <div><label for="pwd">Password</label>
+
+    <div><label for="pwd1">Password</label>
         <input type="password" name="pwd" required></div>
-    
-    <div><label for="firstname">First Name</label>
-        <input type="text" name="firstname" required></div>
-        
-        
+
+    <div><label for="pwd2">Repeat-Password</label>
+        <input type="password" name="rep-pwd" required></div>
+
+    <div><button type="submit" name="submitsign">Submit</button></div>
+
     </form>
-    
 </div>
-    -->
+
 </body>
 </html>
