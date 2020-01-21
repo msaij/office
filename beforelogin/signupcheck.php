6ed2d9df7d510e313a979150a4475ee335fa11d6
MSaiJagapathi <msaij@users.noreply.github.com>
2020-01-21 12:42:03 +0530

    hashing password

--- beforelogin/signupcheck.php
+++ beforelogin/signupcheck.php
@@ -8,9 +8,11 @@
   $pn=mysqli_real_escape_string($conn,$_REQUEST['phonenumber']);
   $mail=mysqli_real_escape_string($conn,$_REQUEST['email']);
   $pass=mysqli_real_escape_string($conn,$_REQUEST['pwd']);
+  $hashp=password_hash($pass,PASSWORD_DEFAULT);
   $passrep=mysqli_real_escape_string($conn,$_REQUEST['rep-pwd']);
+  $hashpr=password_hash($passrep,PASSWORD_DEFAULT);
 
-  $sql="insert into signup (cid,loc_id,firstname,lastname,phone,email,pass,passrep) values ('$cid','$lid','$fn','$ln','$pn','$mail','$pass','$passrep')";
+  $sql="insert into signup (cid,loc_id,firstname,lastname,phone,email,pass,passrep) values ('$cid','$lid','$fn','$ln','$pn','$mail','$hashp','$hashpr')";
 
   if(mysqli_query($conn,$sql))
   {
@@ -18,7 +20,7 @@
   }
   else
   {
-    # header ("location: signup.php");
+    header ("location: signup.php");
   }
   mysqli_close($conn);
 
