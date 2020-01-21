c57eb69956595de2bbce3fc48492106489cdd762
MSaiJagapathi <msaij@users.noreply.github.com>
2020-01-21 13:11:37 +0530

    .

--- beforelogin/signupcheck.php
+++ beforelogin/signupcheck.php
@@ -8,10 +8,8 @@
   $pn=mysqli_real_escape_string($conn,$_REQUEST['phonenumber']);
   $mail=mysqli_real_escape_string($conn,$_REQUEST['email']);
   $pass=mysqli_real_escape_string($conn,$_REQUEST['pwd']);
-  $hashp=password_hash($pass,PASSWORD_DEFAULT);
   $passrep=mysqli_real_escape_string($conn,$_REQUEST['rep-pwd']);
-  $hashpr=password_hash($passrep,PASSWORD_DEFAULT);
-
+  
   $sql="insert into signup (cid,loc_id,firstname,lastname,phone,email,pass,passrep) values ('$cid','$lid','$fn','$ln','$pn','$mail','$pass','$passrep')";
   if(mysqli_query($conn,$sql))
   {
