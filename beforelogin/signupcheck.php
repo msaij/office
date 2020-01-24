26fb7bae1b78e1fc835da3d3d5933b40380b05e1
MSaiJagapathi <msaij@users.noreply.github.com>
2020-01-24 12:57:33 +0530

    .

--- beforelogin/signupcheck.php
+++ beforelogin/signupcheck.php
@@ -1,24 +1,28 @@
 <?php
 
   require 'db_config.php';
-  $cid=mysqli_real_escape_string($conn,$_REQUEST['compid']);
-  $lid=mysqli_real_escape_string($conn,$_REQUEST['locid']);
-  $fn=mysqli_real_escape_string($conn,$_REQUEST['firstname']);
-  $ln=mysqli_real_escape_string($conn,$_REQUEST['lastname']);
-  $un=mysqli_real_escape_string($conn,$_REQUEST['username']);
-  $pn=mysqli_real_escape_string($conn,$_REQUEST['phonenumber']);
-  $mail=mysqli_real_escape_string($conn,$_REQUEST['email']);
-  $pass=mysqli_real_escape_string($conn,$_REQUEST['pwd']);
-  $passrep=mysqli_real_escape_string($conn,$_REQUEST['rep-pwd']);
+  $cid=mysqli_real_escape_string($conn,$_POST['compid']);
+  $lid=mysqli_real_escape_string($conn,$_POST['locid']);
+  $fn=mysqli_real_escape_string($conn,$_POST['firstname']);
+  $ln=mysqli_real_escape_string($conn,$_POST['lastname']);
+  $un=mysqli_real_escape_string($conn,$_POST['username']);
+  $pn=mysqli_real_escape_string($conn,$_POST['phonenumber']);
+  $mail=mysqli_real_escape_string($conn,$_POST['email']);
+  $pass=mysqli_real_escape_string($conn,$_POST['pwd']);
+  $passrep=mysqli_real_escape_string($conn,$_POST['rep-pwd']);
 
-  $sql="insert into signup (cid,loc_id,firstname,lastname,username,phone,email,pass,passrep) values ('$cid','$lid','$fn','$ln','$un','$pn','$mail','$pass','$passrep')";
-  if(mysqli_query($conn,$sql))
+  $sqlcheck="select username,cid,loc_id from signup where username=$un or cid=$cid and loc_id=$lid";
+  if(mysqli_num_rows($conn,$sqlcheck)==0)
   {
-    header ("location: login_page.php");
-  }
-  else
-  {
-     header ("location: signup.php");
+    $sql="insert into signup (cid,loc_id,firstname,lastname,username,phone,email,pass,passrep) values ('$cid','$lid','$fn','$ln','$un','$pn','$mail','$pass','$passrep')";
+    if(mysqli_query($conn,$sql))
+    {
+      header ("location: login_page.php");
+    }
+    else
+    {
+       header ("location: signup.php");
+    }
   }
   mysqli_close($conn);
 
