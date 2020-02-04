7e064ab22ca45cceddaf54087d8c77701ef9e85a
MSaiJagapathi <msaij@users.noreply.github.com>
2020-02-04 19:05:18 +0530

    redoing my ***

--- admin_assoc/loggedin/home.php
+++ admin_assoc/loggedin/home.php
@@ -1,6 +1,5 @@
 <?php
 include 'loggedbasepage.php';
-require_once 'homeparts/listoforders.php';
 ?>
 
 <!DOCTYPE html>
@@ -12,36 +11,6 @@ require_once 'homeparts/listoforders.php';
     <script src="jcss_loggedin/ordersort.js" charset="utf-8"></script>
   </head>
   <body>
-
-<!-- sorting -->
-    <div class="sorting">
-      <h5 style="margin:5px">Sorting parameters-</h5>
-      <div class="parameters">
-        <form id="parameters">
-            <input type="text" name="byname" id="p1" placeholder="Product name.." onkeyup="sortbyname()"/>
-            <input type="month" name="bymonth" id="p2" onkeyup="sortbymonth()" value="2020-01"/>
-        </form>
-      </div>
-    </div>
-
-<!-- orders -->
-    <div class="listoforders">
-      <h6 style="margin:5px">Orders :</h6>
-        <table id="orderslist">
-            <tr>
-                <th>Username</th>
-                <th>Name</th>
-                <th>Category</th>
-                <th>Brand</th>
-                <th>Price</th>
-                <th>Case-quantity</th>
-                <th>Case-required</th>
-                <th>Pieces-required</th>
-                <th>Delivery date</th>
-            </tr>
-           <?php  $yu=orders();?>
-        </table>
-    </div>
 
   </body>
 </html>
